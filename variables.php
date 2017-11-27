fa6f242a43e5e18c51a5e92af23585b95ee3d8bf
fabien <fabienly@gmail.com>
2017-11-27 16:23:53 +0100

    création du fichier variables.php pour découvrir le fonctionnement des variables

---
+++ variables.php
@@ -0,0 +1,29 @@
+<?php
+
+$ageDuVisiteur = 20;
+$prix = 25.8;
+$message = "Bonjour!";
+$message = 'bonjour';
+$estcevrai = true;
+$estcevrai = false;
+
+echo "Le visiteur à $ageDuVisiteur";
+
+echo 'Le visiteur a ' . $ageDuVisiteur . ' ans';
+
+echo $prix;
+
+$nombre = 2 + 4; // $nombre prend la valeur 6
+$nombre = 5 - 1; // $nombre prend la valeur 4
+$nombre = 3 * 5; // $nombre prend la valeur 15
+$nombre = 10 / 2; // $nombre prend la valeur 5
+
+// Allez on rajoute un peu de difficulté
+$nombre = 3 * 5 + 1; // $nombre prend la valeur 16
+$nombre = (1 + 2) * 2; // $nombre prend la valeur 6
+
+$nombre = 10;
+
+$resultat = ($nombre + 5) * $nombre; // $resultat prend la valeur 150
+
+?>
