ab8523ff50d77f4b05397bf938d5d51e17941bd7
fabien <fabienly@gmail.com>
2017-11-30 14:48:45 +0100

    Touch boucles.php : exemples boucles while et for

---
+++ boucles.php
@@ -0,0 +1,15 @@
+<?php
+
+$nombre_de_lignes = 1;
+
+while ($nombre_de_lignes <= 10)
+{
+    echo 'Je ne dois pas regarder les mouches voler quand j\'apprends le PHP.<br />';
+    $nombre_de_lignes++; // $nombre_de_lignes = $nombre_de_lignes + 1
+}
+
+for ($nombre_de_lignes = 1; $nombre_de_lignes <= 20; $nombre_de_lignes++)
+{
+    echo 'Ceci est la ligne n°' . $nombre_de_lignes . '<br />';
+}
+?>
