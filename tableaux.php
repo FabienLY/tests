62d6a16fe006367a55219b9bbec15428fec1f95d
fabien <fabienly@gmail.com>
2017-12-04 11:52:16 +0100

    Touch tableaux.php : création, gestion et recherche dans les tableaux

---
+++ tableaux.php
@@ -0,0 +1,68 @@
+<?php
+
+// La fonction array permet de créer un array
+$prenoms = array ('François', 'Michel', 'Nicole', 'Véronique', 'Benoît');
+
+// Possible de construire le tableau manuellement
+$prenoms[0] = 'François';
+$prenoms[1] = 'Michel';
+$prenoms[2] = 'Nicole';
+
+// On crée notre array $coordonnees
+$coordonnees = array (
+    'prenom' => 'François',
+    'nom' => 'Dupont',
+    'adresse' => '3 Rue du Paradis',
+    'ville' => 'Marseille');
+
+// Puis on fait une boucle pour tout afficher :
+for ($numero = 0; $numero < 5; $numero++)
+{
+    echo $prenoms[$numero] . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
+}
+
+// LA boucle foreach permet de faire la meme chose quelques soit la taille du tableau
+foreach($prenoms as $element)
+{
+    echo $element . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
+}
+// On peut aussi récupérer et afficher la clé associés aux cases du tableau
+foreach($coordonnees as $cle => $element)
+{
+    echo '[' . $cle . '] vaut ' . $element . '<br />';
+}
+/*La commande print_r permet d'afficher rapidement un array pour du debug pas de l'affiche client
+Pour avoir un affichage correct il faut utiliser la balise HTML <pre> */
+
+echo '<pre>';
+print_r($coordonnees);
+echo '</pre>';
+
+//Utilisation de la fonction array_key_exists pour rechercher si une clé est présente dans un array
+if (array_key_exists('nom', $coordonnees))
+{
+    echo 'La clé "nom" se trouve dans les coordonnées !<br />';
+}
+if (array_key_exists('pays', $coordonnees))
+{
+    echo 'La clé "pays" se trouve dans les coordonnées !<br />';
+}
+//Avec in_array recherche d'une valeur dans le tableau
+$fruits = array ('Banane', 'Pomme', 'Poire', 'Cerise', 'Fraise', 'Framboise');
+
+if (in_array('Myrtille', $fruits))
+{
+    echo 'La valeur "Myrtille" se trouve dans les fruits !<br />';
+}
+if (in_array('Cerise', $fruits))
+{
+    echo 'La valeur "Cerise" se trouve dans les fruits !<br />';
+}
+// Récupérer la valeur de la clé associée a une valeur de l'array
+$position = array_search('Fraise', $fruits);
+echo '"Fraise" se trouve en position ' . $position . '<br />';
+
+$position = array_search('Banane', $fruits);
+echo '"Banane" se trouve en position ' . $position;
+
+?>
